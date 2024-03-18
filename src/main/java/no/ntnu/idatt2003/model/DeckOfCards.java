3d4fd2ce2614cc4f29ffc2af97799956a8a399e6
Snake727 <markusnikolai727@gmail.com>
2024-03-18 22:49:10 +0100

    Coded task 2 for DeckOfCards class.

--- src/main/java/no/ntnu/idatt2003/model/DeckOfCards.java
+++ src/main/java/no/ntnu/idatt2003/model/DeckOfCards.java
@@ -1,4 +1,30 @@
 package no.ntnu.idatt2003.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a deck of cards. A deck of cards has 52 cards, 13 of each suit.
+ *
+ * @version 0.1.0
+ * @author Snake727
+ */
 public class DeckOfCards {
+  private final char[] suit = {'S', 'H', 'D', 'C'};
+  private final List<PlayingCard> deck;
+
+  public DeckOfCards() {
+    deck = new ArrayList<>();
+    // Create 52 cards
+    for (char s : suit) {
+      for (int j = 1; j <= 13; j++) {
+        PlayingCard card = new PlayingCard(s, j);
+        deck.add(card);
+      }
+    }
+  }
+
+  public List<PlayingCard> getDeck() {
+    return deck;
+  }
 }
